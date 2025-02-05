50ecf1dabba699cff8fa5e87211f00aea6b70295
Elvis Wangari <elviswangare@gmail.com>
2025-02-05 13:57:12 +0300

    Restructuring Website

---
+++ dotone/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          'primary-black': '#000000',
+          'primary-blue': '#0056D2',
+          'sky-blue': '#87CEFA',
+          'neutral-gray': '#B0B0B0',
+        },
+        boxShadow: {
+          'custom': '0 4px 6px -1px rgba(0, 86, 210, 0.1), 0 2px 4px -1px rgba(0, 86, 210, 0.06)',
+          'custom-lg': '0 10px 15px -3px rgba(0, 86, 210, 0.1), 0 4px 6px -2px rgba(0, 86, 210, 0.05)',
+        },
+      },
+    },
+    plugins: [],
+  };
